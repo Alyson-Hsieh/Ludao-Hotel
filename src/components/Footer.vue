ce3b27e11daff0c3f48ae536c66b11f533e45e89
Alyson-Hsieh <hla32ap525@gmail.com>
2025-04-21 11:37:28 +0800

    test

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,8 +3,7 @@
     <nav class="max-w-8xl mx-auto flex justify-between items-center px-4 py-6">
       <h1 class="font-serif tracking-wide border-b">Back to top</h1>
       <p class="font-serif">
-        電話：082-320048 傳真：082-375936 手機：0911-665-175
-        地址：金門縣金寧鄉下埔下90號　
+        電話：082-320048 傳真：082-375936 手機：0911-665-175 地址：金門縣金寧鄉下埔下90號
       </p>
     </nav>
   </header>
