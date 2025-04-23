c070dc24986239136b1289ce04e289cc28fc98c1
Alyson-Hsieh <hla32ap525@gmail.com>
2025-04-24 01:57:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  publicPath:'./',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
